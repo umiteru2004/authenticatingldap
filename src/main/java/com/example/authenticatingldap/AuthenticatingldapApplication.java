dcb9f1b870d0b15daa82d20a3b141ba41d16081c
umiteru2004 <umiteru2004@gmail.com>
2026-06-02 19:52:16 +0900

    first commit

---
+++ src/main/java/com/example/authenticatingldap/AuthenticatingldapApplication.java
@@ -0,0 +1,13 @@
+package com.example.authenticatingldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticatingldapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticatingldapApplication.class, args);
+	}
+
+}
