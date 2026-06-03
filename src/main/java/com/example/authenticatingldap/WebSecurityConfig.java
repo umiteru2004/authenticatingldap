60f7861c85ab4b2c464951bec784abf22f3a2d20
umiteru2004 <umiteru2004@gmail.com>
2026-06-03 13:06:03 +0900

    Spring Security をセットアップする

---
+++ src/main/java/com/example/authenticatingldap/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.example.authenticatingldap;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.ldap.core.support.BaseLdapPathContextSource;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.ldap.LdapPasswordComparisonAuthenticationManagerFactory;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            BaseLdapPathContextSource contextSource) {
+        LdapPasswordComparisonAuthenticationManagerFactory factory = new LdapPasswordComparisonAuthenticationManagerFactory(
+                contextSource, new BCryptPasswordEncoder());
+        factory.setUserDnPatterns("uid={0},ou=people");
+        return factory.createAuthenticationManager();
+    }
+
+}
